24a817290667bc64546b003337aad16d2799265a
ronakrpanchal <124129862+ronakrpanchal@users.noreply.github.com>
2023-06-15 14:44:01 +0530

    Delete Humankind directory

--- Humankind/script.js
+++
@@ -1,8 +0,0 @@
- // Add your JavaScript code here
-    // Example: Display a welcome message on button click
-    window.addEventListener('DOMContentLoaded', (event) => {
-        const btnPrimary = document.querySelector('.btn-primary');
-        btnPrimary.addEventListener('click', () => {
-          alert('Thank you for your donation!');
-        });
-      });
