a4d4d4adb680f4211f25e221f56e0daf81835d6d
ronakrpanchal <124129862+ronakrpanchal@users.noreply.github.com>
2023-06-18 11:38:56 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -148,3 +148,22 @@
 
 
 })(jQuery);
+
+
+// Owl-Carousel
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+})
